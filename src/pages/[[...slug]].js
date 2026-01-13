e2fd81ea1019bdb1499a2d996ab706d218ae0189
Robert Gorman <rbtgorman@gmail.com>
2026-01-13 12:02:17 -0500

    update slug

--- src/pages/[[...slug]].js
+++ src/pages/[[...slug]].js
@@ -47,7 +47,6 @@ export function getStaticPaths() {
 
 export async function getStaticProps({ params }) {
     const data = allContent();
-    // Fix: Handle undefined params for root route
     const slug = params?.slug || [];
     const urlPath = '/' + slug.join('/');
     const props = await resolveStaticProps(urlPath, data);
